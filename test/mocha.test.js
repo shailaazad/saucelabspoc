be24349d332f1ef5c9d1f88d83ebb3c59908de60
Luke Fender <lfender6445@gmail.com>
2018-07-19 21:14:19 -0400

    updates

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -7,8 +7,7 @@ describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
         browser.url('/');
         var title = browser.getTitle();
-        assert.equal(title, 'TBS Home Page | TBS.co');
+        assert.equal(title, 'TBS Home Page | TBS.com');
     });
 
 });
-
