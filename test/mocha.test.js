2dd8a3f81e9913d6e0dec1cb3fd8d40e1da9a499
shaila <shaila.azad@turner.com>
2018-07-19 23:23:41 -0400

    add new test

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -10,22 +10,27 @@ describe('webdriver.io page', function() {
       assert.equal(title, 'TBS Home Page | TBS.com');
   });
 
-  describe('movie player', function() {
+  describe('movie player', function()  {
     it('should goto the first movie url', function (){
       browser.url('/movies');
       const selector = '.icon-tbstnt_icon_play'
       browser.click(selector)
-      const actual = browser.getUrl();
-      const expected = '/movies/'
       // console.log('actual', actual)
-      const match = actual.match(expected).length > 0
-      assert.equal(match, true)
+      //setTimeout(function(){
+        const actual = browser.getUrl();
+        const expected = '/movies/'
+        const match = actual.match(expected).length > 0
+        assert.equal(match, true)
+      //},1000)
+      
     })
+    it('plays freeview movie', function () {
+      const selector = '.icon-tbstnt_icon_play'
+      browser.click(selector)
 
+    })
   })
 
-  // it('', function () {
 
-  // })
 
 });
